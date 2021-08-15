66b73c45cf76786fe2a84dee28898341bbb1c389
tangx <uyinn@live.com>
2021-08-15 17:13:15 +0800

    05. Vue3 响应行为

--- src/components/Vue3Reaction.vue
+++ src/components/Vue3Reaction.vue
@@ -4,7 +4,8 @@
       注意: `lang="ts"` 的时候， 是不能增删对象属性的。 因为 typescript
       是强类型语言。 使用了 `window.Proxy`
     </n-tag>
-
+    <p>1. 通过 Proxy 代理: 拦截对象中任意变化的属性，增删改查</p>
+    <p>2. 通过 Reflect 反射 对 **源对象** 的操作</p>
     <h2>name: {{ person.name }}</h2>
     <h2 v-show="person.age">age: {{ person.age }}</h2>
     <h2 v-if="person.gender">name: {{ person.gender }}</h2>
