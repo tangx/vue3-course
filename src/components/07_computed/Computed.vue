8bcb98782d9f983c7b25723e5f2851a634139fd4
tangx <uyinn@live.com>
2021-08-16 14:23:58 +0800

    chore

--- src/components/07_computed/Computed.vue
+++ src/components/07_computed/Computed.vue
@@ -1,17 +1,18 @@
 <template>
   <n-card title="07. 计算属性">
     <div>
-      first name: <input type="text" v-model="person.firstName" />
-      <br />
-      last name: <input type="text" v-model="person.lastName" />
-      <br />
-      fullname01:
+      first name:
+      <input type="text" v-model="person.firstName" />
+      <br />last name:
+      <input type="text" v-model="person.lastName" />
+      <br />fullname01:
       <input type="text" v-model="person.fullname01" />
     </div>
     <div>
-      fullname readonly:<input type="text" v-model="fullnameReadonly" />
-      <br />
-      fullname changable:<input type="text" v-model="fullnameChangeable" />
+      fullname readonly:
+      <input type="text" v-model="fullnameReadonly" />
+      <br />fullname changable:
+      <input type="text" v-model="fullnameChangeable" />
     </div>
   </n-card>
 </template>
@@ -39,6 +40,9 @@ export default {
     });
 
     // 虽然成功赋值了， 但是不能显示动态计算
+    // 因为这里 .value 了。 所以赋值是静态值。 
+    //   1. 可以使用 toRef 方式解决
+    //   2. 或者使用 watchEffect 解决
     // person.fullname01 = computed(() => {
     //   return person.firstName + " " + person.lastName;
     // }).value;
